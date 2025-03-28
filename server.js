f02c732cae968b3ea5d3e36230214a74a0bab4e4
Vinay Pandey <vinay13hs@gmail.com>
2025-03-28 15:19:10 +0530

    third version

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
-
+require("dotenv").config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); //req.body
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req,res)=>{
     res.send("Welcome to my Hotel!")
@@ -16,5 +17,6 @@ const menuItemRoutes =require("./routes/menuItemRoutes")
 // use the routers
 app.use("/person",personRoutes)
 app.use("/menu",menuItemRoutes)
+
 
 app.listen(3000, ()=> console.log("listening on port 3000"));
